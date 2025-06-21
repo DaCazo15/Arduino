10858a541c85ef46ffdeb89c04a3a49fcfb6df8f
Daniel Cazorla <148243837+DaCazo15@users.noreply.github.com>
2025-06-21 17:36:59 -0400

    Add files via upload

---
+++ motor.ino
@@ -0,0 +1,20 @@
+#include "AccelStepper.h"
+
+#define dirPin 2
+#define stepPin 3
+#define motorInterfaceType 1
+
+AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
+
+void setup() {
+  // Establece la velocidad máxima en pasos por segundo:
+  stepper.setMaxSpeed(1000);
+}
+
+void loop() {
+  // Establece la velocidad en pasos por segundo:
+  stepper.setSpeed(400);
+  // Mueve el motor con una velocidad constante 
+  // Establecida por setSpeed():
+  stepper.runSpeed();
+}
